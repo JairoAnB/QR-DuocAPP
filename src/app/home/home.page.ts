ab8ae4b18bbcc190abb40ce0e147bbeb8a4e984d
Jairo <jairoandregonzales@gmail.com>
2024-09-15 19:24:46 -0300

    Añado detalles finales a la pagina de login, recalcando pequeña verificacion basica y imagenes.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+correo: string ='';
+password: string ='';
+  constructor(private toastController: ToastController) {}
 
-  constructor() {}
+  async mostrarError(){
+    const error= await this.toastController.create({
+      message: 'Verifica que tu contraseña sea valida o que tu correo sea de DUOC UC',
+      duration: 3000,
+      color: 'danger'
+    });
+    error.present();
+  }
+  async mostrarValidacion(){
+    const validacion= await this.toastController.create({
+      message: 'Correo y contraseña validos, siga con su camino, bienvenid@!',
+      duration: 3000,
+      color: 'success'
+    });
+    validacion.present();
+  }
 
+  verificarDatos(){
+    const correoL = this.correo.trim().toLowerCase().replace(/\s+/g, "");
+    const passwordL = this.password.trim().toLowerCase().replace(/\s+/g, "");
+    if(correoL.includes("@duocuc") && passwordL.length >= 6){
+      this.mostrarValidacion();
+    }else{
+      this.mostrarError();
+    }
+  }
 }
