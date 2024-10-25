6f77d07ad3974f0ddb446eae7efe61ed571f6f9c
LeVenegass <le.venegass@duocuc.cl>
2024-10-25 15:56:34 -0300

    Poniendo bello al api

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -1,21 +1,27 @@
-
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs'; 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-
   URI: string = '';
   apiKey: String = 'd1bb71ed1150e258dace9a340aae1baf';
-
-  constructor( private httpClient : HttpClient) {
+  
+  constructor(private httpClient: HttpClient) {
     this.URI = 'https://api.openweathermap.org/data/2.5/weather';
-   }
-   getWeather(cityName: string, countryCode: string): Observable<any> {
+  }
+  
+  getWeather(cityName: string, countryCode: string): Observable<any> {
     const url = `${this.URI}?q=${cityName},${countryCode}&appid=${this.apiKey}&units=metric`;
     return this.httpClient.get(url);
-   }
+  }
+
+
+getWeatherByCoordinates(lat: number, lon: number): Observable<any> {
+  const url = `${this.URI}?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
+  return this.httpClient.get(url);
+
+}
 }
